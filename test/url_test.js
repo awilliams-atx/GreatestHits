b07fbdcdcd55f790014563efc5336d71ae8468b4
Andrew Williams <andrew.williams.atx@gmail.com>
2016-08-15 14:04:46 -0400

    url_test factors out setup code into helper functions, tests Url::find, Url::insert

--- test/url_test.js
+++ test/url_test.js
@@ -9,12 +9,30 @@ const should = chai.should();
 const sqlite3 = require('sqlite3');
 
 describe('Url model', function () {
-  var db, insertGoogle, insertYahoo;
-  beforeEach(function () {
+  var db, dropAndCreateTableUrls, dropTableUrls, insertGoogle, insertYahoo, selectSqlZoo;
+  beforeEach(() => {
+    dropAndCreateTableUrls = function (cb) {
+      db = new sqlite3.Database('./data.db', () => {
+        db.serialize(() => {
+          db.run('DROP TABLE IF EXISTS urls;').run('CREATE TABLE urls (id INTEGER PRIMARY KEY AUTOINCREMENT, long TEXT NOT NULL, desktop TEXT, mobile TEXT, tablet TEXT, createdAt TEXT NOT NULL, updatedAt TEXT NOT NULL);', [], () => {
+            db.close();
+            cb();
+          });
+        });
+      });
+    };
+    dropTableUrls = function (cb) {
+      db = new sqlite3.Database('./data.db', () => {
+        db.run('DROP TABLE IF EXISTS urls;', [], () => {
+          db.close();
+          cb && cb();
+        });
+      });
+    };
     insertGoogle = function (cb) {
-      db = new sqlite3.Database('./data.db', function () {
+      db = new sqlite3.Database('./data.db', () => {
         db.serialize(function () {
-          db.run('DROP TABLE IF EXISTS urls;').run('CREATE TABLE urls (id INTEGER PRIMARY KEY AUTOINCREMENT, long TEXT NOT NULL, desktop TEXT, mobile TEXT, tablet TEXT, createdAt TEXT NOT NULL, updatedAt TEXT NOT NULL);').run("INSERT INTO urls (long, desktop, mobile, tablet, createdAt, updatedAt) VALUES ('http://www.google.com', '54354gdsm', 'fdjs8f98f2', '0sd9fj23', datetime(), datetime());", [], () => {
+          db.run("INSERT INTO urls (long, desktop, mobile, tablet, createdAt, updatedAt) VALUES ('http://www.google.com', '54354gdsm', 'fdjs8f98f2', '0sd9fj23', datetime(), datetime());", [], () => {
             db.close();
             cb();
           });
@@ -22,13 +40,27 @@ describe('Url model', function () {
       });
     };
     insertYahoo = function (cb) {
-      db = new sqlite3.Database('./data.db', function () {
+      db = new sqlite3.Database('./data.db', () => {
         db.run("INSERT INTO urls (long, desktop, mobile, tablet, createdAt, updatedAt) VALUES ('http://www.yahoo.com', '5teff2', 'dsf234f', 'h7j8rwece', datetime(), datetime());", [], () => {
           db.close();
           cb();
         });
       });
     };
+    selectSqlZoo = function (cb) {
+      db = new sqlite3.Database('./data.db', () => {
+        db.get("SELECT * FROM urls WHERE long = 'http://www.sqlzoo.com/';"
+          , [], (err, row) => {
+          if (err) {
+            db.close();
+            return cb(err);
+          } else {
+            db.close();
+            return cb(row);
+          }
+        });
+      });
+    }
   });
   describe('when table does not exist', function () {
     var db;
@@ -40,74 +72,143 @@ describe('Url model', function () {
         });
       });
     });
-    it('::find rejects with error', function (done) {
-      Url.find({tableName: 'urls', Constructor: Url, id: 5, quiet: true}).should.eventually.be.rejectedWith(Error).notify(done);
+    it('::find rejects with error', (done) => {
+      Url.find({
+        tableName: 'urls',
+        Constructor: Url,
+        id: 5,
+        quiet: true
+      }).should.eventually.be.rejectedWith(Error).notify(done);
     });
-    it('::update rejects with error', function (done) {
-      Url.update({tableName: 'urls', set: { long: 'http://www.google.com/' }, quiet: true}).should.eventually.be.rejectedWith(Error).notify(done);
+    it('::update rejects with error', (done) => {
+      Url.update({
+        tableName: 'urls',
+        set: { long: 'http://www.google.com/' },
+        quiet: true
+      }).should.eventually.be.rejectedWith(Error).notify(done);
     });
-    it('::insert rejects with error', function (done) {
-      Url.insert({tableName: 'urls', quiet: true, attributes: { long: 'http://www.google.com/' }}).should.eventually.be.rejectedWith(Error).notify(done);
+    it('::insert rejects with error', (done) => {
+      Url.insert({
+        tableName: 'urls',
+        quiet: true,
+        attributes: { long: 'http://www.google.com/'}
+      }).should.eventually.be.rejectedWith(Error).notify(done);
     });
-    it('::where rejects with error', function (done) {
-      Url.where({tableName: 'urls', where: { long: 'http://www.google.com/' }, quiet: true}).should.eventually.be.rejectedWith(Error).notify(done);
+    it('::where rejects with error', (done) => {
+      Url.where({
+        tableName: 'urls',
+        where: { long: 'http://www.google.com/' },
+        quiet: true
+      }).should.eventually.be.rejectedWith(Error).notify(done);
     });
-    it('#insert rejects with error', function (done) {
-      let url = new Url({long: 'http://www.google.com/'});
-      url.insert({quiet: true, done: true}).should.eventually.be.rejectedWith(Error).notify(done);
+    it('#insert rejects with error', (done) => {
+      let url = new Url({
+        long: 'http://www.google.com/'
+      });
+      url.insert({
+        quiet: true,
+        done: true
+      }).should.eventually.be.rejectedWith(Error).notify(done);
     });
-    it('#update rejects with error', function (done) {
-      let url = new Url({id: 1, long: 'http://www.google.com/'});
-      url.update({quiet: true}).should.eventually.be.rejectedWith(Error).notify(done);
+    it('#update rejects with error', (done) => {
+      let url = new Url({
+        id: 1,
+        long: 'http://www.google.com/'
+      });
+      url.update({
+        quiet: true
+      }).should.eventually.be.rejectedWith(Error).notify(done);
     });
   });
   describe('when table is empty', function () {
     var db;
-    before(function (done) {
-      db = new sqlite3.Database('./data.db', function () {
-        db.serialize(function () {
-          db.run('DROP TABLE IF EXISTS urls').run('CREATE TABLE urls (id INTEGER PRIMARY KEY AUTOINCREMENT);', [], (err) => {
+    beforeEach(done => {
+      dropAndCreateTableUrls(done);
+    });
+    after(done => {
+      dropTableUrls(done);
+    });
+    it('::find returns null', (done) => {
+      Url.find({
+        Constructor: Url,
+        tableName: 'urls',
+        id: 1,
+        quiet: true
+      }).should.eventually.equal(null).notify(done);
+    });
+    it('::where returns empty array', (done) => {
+      Url.where({
+        tableName: 'urls',
+        conditions: { long: 'http://www.google.com' },
+        Constructor: Url,
+        quiet: true
+      }).should.eventually.deep.equal([]).notify(done);
+    });
+  });
+  describe('when table has rows', () => {
+    describe('::find', () => {
+      beforeEach(done => {
+        dropAndCreateTableUrls(() => {
+          insertGoogle(() => {
             done();
           });
         });
       });
-    });
-    after(function (done) {
-      db = new sqlite3.Database('./data.db').run('DROP TABLE urls;', [], () => {
-        done();
-      });
-    });
-    it('::find returns null', function (done) {
-      Url.find({Constructor: Url, tableName: 'urls', id: 1, quiet: true}).should.eventually.equal(null).notify(done);
-    });
-    it('::where returns empty array', function (done) {
-      Url.where({tableName: 'urls', conditions: { long: 'http://www.google.com'}, Constructor: Url, quiet: true}).should.eventually.deep.equal([]).notify(done);
-    });
-  });
-  describe('when table has rows', function () {
-    describe('::find', function () {
-      beforeEach(function (done) {
-        insertGoogle(function () { done(); });
-      });
-      it('returns null when id not in table', function (done) {
+      it('returns null when id not in table', (done) => {
         Url.find({tableName: 'urls', Constructor: Url, id: 2, quiet: true}).should.eventually.equal(null).notify(done);
       });
-      it('returns a JS wrapped database object', function (done) {
-        Url.find({tableName: 'urls', Constructor: Url, id: 1, quiet: true}).should.eventually.be.an.instanceof(Url).notify(done);
+      it('returns a JS wrapped database object', (done) => {
+        Url.find({
+          tableName: 'urls',
+          Constructor: Url,
+          id: 1,
+          quiet: true
+        }).should.eventually.be.an.instanceof(Url).notify(done);
       });
-      it('returns the correct object', function (done) {
-        insertYahoo(function () {
+      it('returns the correct object', (done) => {
+        insertYahoo(() => {
           Url.find({
             tableName: 'urls',
             Constructor: Url,
             id: 2,
             quiet: true
-          }).should.eventually.be.an.instanceof(Url).with.deep.property('attributes.id', 2).notify(done);
+          }).should.eventually.be.an.instanceof(Url)
+            .with.deep.property('attributes.id', 2).notify(done);
         });
       });
     });
-    describe('::insert', function () {
-
+    describe('::insert', () => {
+      beforeEach(done => {
+        dropAndCreateTableUrls(() => {
+          Url.insert({
+            tableName: 'urls',
+            attributes: {long: 'http://www.sqlzoo.com/'},
+            quiet: true
+          }).then(() => {
+            done();
+          });
+        });
+      });
+      after(done => {
+        dropTableUrls(done);
+      });
+      after(done => {
+        dropTableUrls(done);
+      });
+      it('inserts a record', (done) => {
+        selectSqlZoo((row) => {
+          row.should.have.deep.property('id', 1);
+          row.should.have.deep.property('long', 'http://www.sqlzoo.com/');
+          done();
+        });
+      });
+      it('automatically inserts createdAt, updatedAt', (done) => {
+        selectSqlZoo(row => {
+          row.createdAt.should.match(/^\d+/);
+          row.updatedAt.should.match(/^\d+/);
+          done();
+        });
+      });
     });
   });
 });
