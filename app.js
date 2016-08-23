a2797e61657e39e637457a27f3928893733d10ec
Andrew Williams <andrew.williams.atx@gmail.com>
2016-08-23 15:49:07 -0400

    app serves static index.html from /public

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 
 const express = require('express');
 const morgan = require('morgan');
+const path = require('path');
 const routes = require('./config/routes');
 const bodyParser = require('body-parser');
 const device = require('express-device');
@@ -16,7 +17,13 @@ const App = function (options) {
   }
   app.use(device.capture());
   app.use(bodyParser.json());
+  app.use(express.static('public'));
+
   app.use(routes);
+  app.use('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'public/index.html'));
+  });
+
   return app;
 };
 
