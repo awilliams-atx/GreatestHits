a29504fc743f2997eccbfbf1dbee9040868f69e3
Andrew Williams <andrew.williams.atx@gmail.com>
2016-08-23 18:11:58 -0400

    adds node-sass and node-sass-middleware

--- app.js
+++ app.js
@@ -1,23 +1,28 @@
 'use strict';
 
+const bodyParser = require('body-parser');
+const device = require('express-device');
 const express = require('express');
 const morgan = require('morgan');
 const path = require('path');
 const routes = require('./config/routes');
-const bodyParser = require('body-parser');
-const device = require('express-device');
+const sassMiddleware = require('node-sass-middleware');
 
 const App = function (options) {
   let app = express();
   app.use((req, res, next) => {
     next();
   });
-  if (!options.quiet) {
-    app.use(morgan('dev'));
-  }
+  if (!options.quiet) { app.use(morgan('dev')) }
   app.use(device.capture());
   app.use(bodyParser.json());
-  app.use(express.static('public'));
+
+  app.use(sassMiddleware({
+    src: __dirname + '/sass',
+    dest: __dirname + '/public/stylesheets',
+    prefix: '/stylesheets'
+  }));
+  app.use(express.static(path.join(__dirname, 'public')));
 
   app.use(routes);
   app.use('*', (req, res) => {
