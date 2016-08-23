64ace149f11a16ab05fa0cd6282bcb53be95192e
Andrew Williams <andrew.williams.atx@gmail.com>
2016-08-23 14:36:24 -0400

    adds webpack.config.js

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+var path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.join(__dirname, 'public', 'javascripts'),
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015', 'react']
+        }
+      }
+    ]
+  },
+  devtool: 'source-map'
+};
