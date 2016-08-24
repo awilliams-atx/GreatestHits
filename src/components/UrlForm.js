97dcd6cca578d8df899177cb00cc7556b32ab00e
Andrew Williams <andrew.williams.atx@gmail.com>
2016-08-23 22:37:50 -0400

    adds url-form modal

--- src/components/UrlForm.js
+++ src/components/UrlForm.js
@@ -1,19 +1,52 @@
 import React, { Component } from 'react';
 
 class UrlForm extends Component {
-  getInitialState () {
-    return {
+  constructor () {
+    super();
+    this.state = {
       desktop: '',
       mobile: '',
       tablet: ''
-    }
+    };
   }
+
   render () {
     return (
-      <form>
-        <input value={this.state.desktop} />
-      </form>
+      <div id='modal-background'>
+        <aside id='modal' className='group' >
+          <form>
+            <label>Desktop
+              <input value={this.state.desktop}
+                onChange={this.onDesktopChange}/>
+            </label>
+            <label>Mobile
+              <input value={this.state.mobile}
+                onChange={this.onMobileChange}/>
+            </label>
+            <label>Tablet
+              <input value={this.state.tablet}
+                onChange={this.onTabletChange}/>
+            </label>
+            <div id='modal-buttons' className='group'>
+              <button>Cancel</button>
+              <button>Submit</button>
+            </div>
+          </form>
+        </aside>
+      </div>
     );
+  }
+
+  onDesktopChange () {
+    this.setState({ desktop: e.target.value });
+  }
+
+  onMobileChange () {
+    this.setState({ mobile: e.target.value });
+  }
+
+  onTabletChange () {
+    this.setState({ tablet: e.target.value });
   }
 }
 
